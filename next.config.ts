b52ec9157d9a3c0646c59748b0919bbc002c13e9
Shahank Kolakaluri <shashank.kolakaluri@gmail.com>
2026-06-22 20:25:58 -0700

    fix images.remotePatterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
-// next.config.ts
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["uiqsapqeamvjotcihlgc.supabase.co"], // <-- Add your Supabase project domain here
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uiqsapqeamvjotcihlgc.supabase.co',
+      },
+    ],
   },
 };
 
